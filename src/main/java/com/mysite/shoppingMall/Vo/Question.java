ae42244110008ae63d9f862d674da429673cf42d
leehaneul623 <dyj03216@naver.com>
2022-08-08 13:42:30 +0900

    question 연결 중

---
+++ src/main/java/com/mysite/shoppingMall/Vo/Question.java
@@ -0,0 +1,31 @@
+package com.mysite.shoppingMall.Vo;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Setter
+@Getter
+public class Question {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private LocalDateTime createDate;
+
+    @Column(length = 200)
+    private String subject;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    @OneToMany(mappedBy = "question", cascade = CascadeType.REMOVE)
+    private List<QuestionAnswer> questionAnswerList;
+
+    @ManyToOne
+    private MallUser mallUser;
+}
